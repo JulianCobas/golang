81055b1857f1af16f5462b98e07cb2ba13987b24
julian.cobas <julian.cobas@bbva.com>
2018-01-22 09:01:26 +0100

    Adding pointers and loop information

---
+++ 09_loops/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// init-condition-post
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i)
+	}
+
+	// nested loops
+	fmt.Println("Nested loops")
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Println(i, " - ", j)
+		}
+	}
+
+	// while
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	// for break
+	i = 0
+	for {
+		fmt.Println(i)
+		if i >= 10 {
+			break
+		}
+		i++
+	}
+
+	// for continue
+	i = 0
+	for {
+		i++
+		if i%2 == 0 {
+			// Skips the rest of iteration and goes to next iteration
+			continue
+		}
+
+		fmt.Println(i)
+		if i >= 50 {
+			break
+		}
+
+	}
+}
