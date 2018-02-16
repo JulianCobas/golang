337d60af37b8ee037070f34eec643e55980a4ea5
julian.cobas <julian.cobas@bbva.com>
2018-02-16 09:35:03 +0100

    OOP, composition and embedding

--- 15_data-structures/04_structs/main.go
+++ 15_data-structures/04_structs/main.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 // Struct definition
-type person struct {
+type Person struct {
 	first string
 	last  string
 	age   int
+}
+
+type DoubleZero struct {
+	//anonymous type. DoubleZero has directly access to Person fields
+	Person
+	First         string
+	LicenseToKill bool
 }
 
 // We can create our own types
@@ -16,8 +23,16 @@ func main() {
 	var myAge foo
 
 	myAge = 15
-	p1 := person{"James", "Bond", 31}
-	p2 := person{"Miss", "Moneypenny", 25}
+
+	p1 := DoubleZero{
+		Person: Person{
+			First: "James",
+			Last:  "Bond",
+			Age:   31},
+		First:         "Double Zero Seven",
+		LicenseToKill: true,
+	}
+	p2 := Person{"Miss", "Moneypenny", 25}
 
 	fmt.Println(p1.first, p1.last, p1.age)
 	fmt.Println(p2.first, p2.last, p2.age)
